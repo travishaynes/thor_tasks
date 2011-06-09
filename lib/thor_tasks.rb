97ce2d7d4f25ba6383c3aa07b75cbc9caa55ea8c
Travis Haynes <travis.j.haynes@gmail.com>
2011-06-08 22:14:07 -0700

    Added install generator.

--- lib/thor_tasks.rb
+++ lib/thor_tasks.rb
@@ -1,2 +1,19 @@
+require 'rails'
+require 'rails/generators'
+
 module ThorTasks
+  
+  class InstallGenerator < Rails::Generators::Base
+    desc "Installs thor tasks into your Rails application."
+    
+    def self.source_root
+      @source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'tasks'))
+    end
+    
+    def copy_tasks
+      copy_file "spec.thor", "lib/tasks/spec.thor"
+      copy_file "spork.thor", "lib/tasks/spork.thor"
+    end
+  end
+  
 end
